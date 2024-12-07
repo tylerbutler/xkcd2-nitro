3aa89f2bab9b9c962afca9933fb67da07404283a
Tyler Butler <tyler@tylerbutler.com>
2024-12-06 21:09:46 -0800

    Disable cache on comic pages

--- src/routes/[id].ts
+++ src/routes/[id].ts
@@ -1,8 +1,8 @@
-export default cachedEventHandler(
+export default eventHandler(
 	async (event) => {
 		const comicId = getRouterParam(event, "id");
 		const html = await renderComicPage(comicId);
 		return html;
 	},
-	{ maxAge: 60 * 60 /* 1 hour */ },
+	// { maxAge: 60 * 60 /* 1 hour */ },
 );
